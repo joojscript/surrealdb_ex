1940f8aefccc8f01db70be4449c5c97896ead84e
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2023-03-31 09:00:49 -0300

    fix: change minimun elixir required version in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SurrealEx.MixProject do
     [
       app: :surreal_ex,
       version: "0.1.0",
-      elixir: "~> 1.15-dev",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       preferred_cli_env: preferred_cli_env()
